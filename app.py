0a5728119194dac1baaf64f74446acac3b04a969
ajgoward <ajgoward@gmail.com>
2020-08-12 12:02:19 +0000

    added the crud functionality for the breakfast collection

--- app.py
+++ app.py
@@ -92,6 +92,48 @@ def dessert():
     return render_template("dessert.html", desserts=mongo.db.desserts.find())
 
 
+# CRUD functionality for the Breakfast collection #
+
+
+@app.route('/add_breakfast')
+def add_breakfast():
+    return render_template("addbreakfast.html")
+
+
+@app.route('/insert_breakfast', methods=["POST"])
+def insert_breakfast():
+    breakfasts = mongo.db.breakfasts
+    breakfasts.insert_one(request.form.to_dict())
+    return redirect(url_for('breakfast'))
+
+
+@app.route('/edit_breakfast/<breakfast_id>')
+def edit_breakfast(breakfast_id):
+    the_breakfast = mongo.db.breakfasts.find_one({
+        "_id": ObjectId(breakfast_id)})
+    return render_template('editbreakfast.html', breakfast=the_breakfast)
+
+
+@app.route('/update_breakfast/<breakfast_id>', methods=["POST"])
+def update_breakfast(breakfast_id):
+    breakfasts = mongo.db.breakfasts
+    breakfasts.update({'_id': ObjectId(breakfast_id)},
+    {
+        'image': request.form.get('image'),
+        'recipe_name': request.form.get('recipe_name'),
+        'time_to_make': request.form.get('time_to_make'),
+        'ingredients': request.form.get('ingredients'),
+        'method': request.form.get('method')
+    })
+    return redirect(url_for('breakfast'))
+
+
+@app.route('/delete_breakfast/<breakfast_id>')
+def delete_breakfast(breakfast_id):
+    mongo.db.breakfasts.remove({'_id': ObjectId(breakfast_id)})
+    return redirect(url_for('breakfast'))
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
