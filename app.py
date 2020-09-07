b17f109c15d331dd601703ebeca3deb5017b0d79
ajgoward <ajgoward@gmail.com>
2020-09-07 15:23:45 +0000

    added comments for readabiltiy and ran python through PEP8 and fixed any issues

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
-#I received the login and register functionality from the code institute tutor support called "Tim"
+# I received the login and register functionality
+# from the code institute tutor support called "Tim"
 
 
 @app.route('/')
@@ -27,20 +28,19 @@ def home():
     return render_template("homepages/home.html")
 
 
-@app.route('/login', methods=['POST',"GET"])
+@app.route('/login', methods=['POST', "GET"])
 def login():
     if request.method == 'POST':
-        #check if username exsits in the database
+        # check if username exsits in the database
         existing_user = mongo.db.users.find_one(
             {'username': request.form.get('username').lower()})
-            #check if the password matches using check_hashword_pass
+        # check if the password matches using check_hashword_pass
         if existing_user:
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                session["user"] = request.form.get("username").lower()
-                return redirect(url_for(
-                    'profile'))
-            #invalid login details
+                    existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    return redirect(url_for('profile'))
+            # invalid login details
             else:
                 flash("Incorrect login details")
         else:
@@ -50,10 +50,10 @@ def login():
     return render_template('homepages/login.html')
 
 
-@app.route('/register', methods=['POST',"GET"])
+@app.route('/register', methods=['POST', "GET"])
 def register():
     if request.method == 'POST':
-         #check if username exsits in the database
+        # check if username exsits in the database
         exsiting_user = mongo.db.users.find_one(
             {'username': request.form.get('username').lower()})
 
@@ -61,7 +61,8 @@ def register():
             # if username exsits display message
             flash('That username already exists!')
             return redirect(url_for('register'))
-        #if username doesnt exsit create dictionary and store the information within
+        # if username doesnt exsit create dictionary and
+        # store the information within
         register = {
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
@@ -76,7 +77,7 @@ def register():
 
 @app.route('/log_out')
 def log_out():
-    #remove session cookies
+    # remove session cookies
     flash("log out successful")
     session.pop("user")
     return redirect(url_for('login'))
@@ -87,7 +88,7 @@ def profile():
         return render_template(
             "homepages/profile.html", recipes=mongo.db.recipes.find())
 
-# recipe pages with function to find the recipes #
+# recipe pages with function to find the recipes
 
 
 @app.route('/breakfast')
