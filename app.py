13366d099c185f28b80dd148d27a47c3424748ef
ajgoward <ajgoward@gmail.com>
2020-09-07 15:08:40 +0000

    fixed register functionality bug on release

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import (
     Flask, render_template, redirect, request, url_for, session, flash)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -18,10 +18,7 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
-# home page
-# I recievd the login functionality from a youtube
-# tutorial you can fiind it here
-# https://www.youtube.com/watch?v=vVx1737auSE#
+#I received the login and register functionality from the code institute tutor support called "Tim"
 
 
 @app.route('/')
@@ -30,41 +27,41 @@ def home():
     return render_template("homepages/home.html")
 
 
-@app.route('/login_page')
-def login_page():
-    return render_template("homepages/index.html")
-
-
-@app.route('/register_page')
-def register_page():
-    return render_template('homepages/signup.html')
-
-
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST',"GET"])
 def login():
-    users = mongo.db.users
-    login_user = users.find_one({'name': request.form['username']})
+    if request.method == 'POST':
+        #check if username exsits in the database
+        existing_user = mongo.db.users.find_one(
+            {'username': request.form.get('username').lower()})
+            #check if the password matches using check_hashword_pass
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                return redirect(url_for(
+                    'profile'))
+            #invalid login details
+            else:
+                flash("Incorrect login details")
+        else:
+            flash("Incorrect login details")
+            return redirect(url_for("login"))
 
-    if login_user:
-        if bcrypt.hashpw(
-            request.form['password'].encode(
-                'utf-8'), login_user['password']) == login_user['password']:
-            session['username'] = request.form['username']
-            return redirect(url_for('profile'))
-
-    flash('invalid login details')
-    return render_template('homepages/index.html')
+    return render_template('homepages/login.html')
 
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['POST',"GET"])
 def register():
     if request.method == 'POST':
+         #check if username exsits in the database
         exsiting_user = mongo.db.users.find_one(
-            {'username': request.form.get['username'].lower()})
+            {'username': request.form.get('username').lower()})
 
         if exsiting_user:
+            # if username exsits display message
             flash('That username already exists!')
             return redirect(url_for('register'))
+        #if username doesnt exsit create dictionary and store the information within
         register = {
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
@@ -72,20 +69,23 @@ def register():
         mongo.db.users.insert_one(register)
 
         session["user"] = request.form.get("username").lower()
-        return redirect(url_for('profile'))
+        return redirect(url_for(
+            'profile'))
+    return render_template('homepages/signup.html')
 
 
 @app.route('/log_out')
 def log_out():
+    #remove session cookies
     flash("log out successful")
-    session.pop("username")
-    return redirect(url_for('home'))
+    session.pop("user")
+    return redirect(url_for('login'))
 
 
 @app.route('/profile')
 def profile():
-    return render_template(
-        "homepages/profile.html", recipes=mongo.db.recipes.find())
+        return render_template(
+            "homepages/profile.html", recipes=mongo.db.recipes.find())
 
 # recipe pages with function to find the recipes #
 
